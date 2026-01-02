761c54b067e7e7a992fc95af02c24ce0a3bfc997
roshni-choudhary <roshnichoudhary2308@gmail.com>
2026-01-02 13:55:58 +0530

    streak 2

---
+++ January/day2.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i+1<n;i++)
+        {
+            if(nums[i]==nums[i+1])
+            return nums[i];        }
+             for(int i=0;i+2<n;i++)
+        {
+            if(nums[i]==nums[i+2])
+            return nums[i];        }
+             for(int i=0;i+3<n;i++)
+        {
+            if(nums[i]==nums[i+3])
+            return nums[i];        }
+    return 0;
+    }
+};
