1017b3f7458125f97b2de29bb380470d7e46c970
roshni-choudhary <roshnichoudhary2308@gmail.com>
2026-01-07 21:14:45 +0530

    day 7

---
+++ January/day7.cpp
@@ -0,0 +1,35 @@
+//1339. Maximum Product of Splitted Binary Tree
+#include<bits/stdc++.h>
+using namespace std;
+  struct TreeNode {
+     int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+class Solution {
+public:
+long long ans=INT_MIN;
+const int mod=1e9+7;
+long long dfs(TreeNode* root)
+{
+    if(root==nullptr)
+    return 0;
+    return root->val+dfs(root->left)+dfs(root->right);
+}
+long long helper(TreeNode* root,long long tsum)
+{
+  if(root==nullptr)
+    return 0;
+    long long sum=root->val+helper(root->left,tsum)+helper(root->right,tsum);
+    ans=max(ans,sum*(tsum-sum));
+    return sum;
+}
+    int maxProduct(TreeNode* root) {
+        long long tsum=dfs(root);
+        helper(root,tsum);
+        return ans%mod; //mod after maximising
+    }
+};
