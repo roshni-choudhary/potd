7b8b47dfaa0e291e2f5d61350ad3c1f369f1d61f
roshni-choudhary <roshnichoudhary2308@gmail.com>
2026-02-01 21:41:06 +0530

    day31

---
+++ January/day31.cpp
@@ -0,0 +1,23 @@
+//744. Find Smallest Letter Greater Than Target
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        char res = letters[0];
+        bool flag = false;
+
+        for (char ch : letters) {
+            if (!flag) {
+                if (ch > target) {
+                    res = ch;
+                    flag = !flag;
+                }
+            } else {
+                if (ch > target && ch < res) res = ch;
+            }
+        }
+
+        return res;
+    }
+};
